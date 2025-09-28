a5d9acdd68f84c0a574a45566331d3dd1611c640
vasuantala049 <vasuantala098@gmail.com>
2025-09-28 13:22:48 +0530

    starting for s3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Replace `react_todoapp` with your repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/react_todoapp/',
+  base: '/',
 })
