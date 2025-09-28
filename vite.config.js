b0e1a61dc66c375ee4845d4b0bf61f4b39a9e9a7
vasuantala049 <vasuantala098@gmail.com>
2025-09-28 13:00:31 +0530

    pakage.json changed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Replace `react_todoapp` with your repo name
 export default defineConfig({
   plugins: [react()],
+  base: '/react_todoapp/',
 })
